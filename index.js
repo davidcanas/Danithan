beb721315925ca221534c7876b1f8fe066fe72bc
SrDCdev <dg0837.dc @gmail.com>
2021-06-03 14:04:52 +0100

    Message Components

--- index.js
+++ index.js
@@ -22,3 +22,12 @@ client.on('error', err => {
         console.error("[Erro Recebido da index.js]: " + err);
   
  })
+ client.on("rawWS", (packet) => {
+     if (packet.t === "INTERACTION_CREATE" && packet.d.type === 3) {
+    console.log("Id da guild" + packet.d.guild_id)
+    console.log("Channel" + packet.d.channel_id)
+    console.log("ID Message" + packet.d.id)  
+
+   client.guilds.get(packet.d.guild_id).channels.get(packet.d.channel_id).messages.get(packet.d.message.id).edit("Olá você clicou numa interação ")
+     }
+ })
