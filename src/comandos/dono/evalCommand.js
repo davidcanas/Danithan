29b8a5b162aa6247fac3c3c57a26a4d71466340c
CanasDev <canasdev@danithan.tk>
2021-06-12 19:02:25 +0100

    Atualizando pro sushi

--- src/comandos/dono/evalCommand.js
+++ src/comandos/dono/evalCommand.js
@@ -20,7 +20,7 @@ constructor(client) {
 }
 async execute(ctx) {
       try {
-        if (ctx.msg.author.id !== '791347446298312724' && ctx.msg.author.id !== '718078381199065150') {
+        if (ctx.msg.author.id !== '791347446298312724' && ctx.msg.author.id !== '718078381199065150' && ctx.msg.author.id !== '852650555254767676') {
             return ctx.msg.channel.createMessage('Apenas meu criador');
         }
         const texto = ctx.args.join(' ')
