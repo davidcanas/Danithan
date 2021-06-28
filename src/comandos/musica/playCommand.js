42eb2a8167300cfb17895cff36dc81eebfe7e41c
Equipa Danithan (CanasDev) <canasdev@danithan.tk>
2021-06-28 12:24:11 +0100

    a

--- src/comandos/musica/playCommand.js
+++ src/comandos/musica/playCommand.js
@@ -24,7 +24,7 @@ module.exports = class tugafmCommand extends Command {
              res = await this.client.manager.search(search, ctx.msg.author);
              // Check the load type as this command is not that advanced for basics
              if (res.loadType === "LOAD_FAILED") throw res.exception;
-             else if (res.loadType === "PLAYLIST_LOADED") throw { message: "Playlists não são suportadas de momento" };
+  
            } catch (err) {
              return ctx.msg.channel.createMessage(`Erro: ${err.message}`);
            }
@@ -35,13 +35,36 @@ module.exports = class tugafmCommand extends Command {
              voiceChannel: ctx.msg.member.voiceState.channelID,
              textChannel: ctx.msg.channel.id,
            });
-         
-           // Connect to the voice channel and add the track to the queue
+           if (res.loadType === 'PLAYLIST_LOADED') {
+            const playlist = res.playlist;
+  
+            for (const track of res.tracks)
+              player.queue.add(track);
+  
+            if (!player.playing)
+              player.play();
+  
+            const embed = new EthanEmbed()
+              .setColor('RANDOM')
+              .setTitle('💿 A Carregar Playlist...')
+              .addField("🎵 Nome da Playlist:", '`' + playlist?.name + '`')
+              .addField("👤 Pedida por:", ctx.msg.author.username + "#" + ctx.msg.author.discriminator)
+              //  .addField(':watch: Duração', `\`${this.client.utils.msToHour(res.playlist?.duration || 0)}\``)
+            
+              .setFooter("💻 | Sistema de música Danithan");
+  
+            const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
+  
+            urlRegex.test(ctx.args[0]) && embed.setURL(ctx.args[0]);
+  
+            ctx.msg.channel.createMessage(embed);
+          } else {
+           // Entra no canal de voz e adiciona a musica pedida a lista 
            player.connect();
            player.queue.add(res.tracks[0]);
          
            // Checks if the client should play the track if it's the first one added
            if (!player.playing) player.play()
-       
+          }     
            return ctx.msg.channel.createMessage(`Adicionando ${res.tracks[0].title} á lista `);
           }}
