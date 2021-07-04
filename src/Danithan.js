78a6dda5400b3e3244d87846f2fae9e1bee00314
CanasDev <canasdev@danithan.tk>
2021-07-04 11:42:01 +0100

    Update Danithan.js

--- src/Danithan.js
+++ src/Danithan.js
@@ -8,7 +8,7 @@ module.exports = class DaniClient extends Client {
         
         this.settings = {
             mongo: process.env.MONGOURI,
-            owners: ["791347446298312724", "852650555254767676"]
+            developers: ["791347446298312724", "852650555254767676"]
         };
 
         this.database = new DanithanDatabase(this.settings.mongo);
