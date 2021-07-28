16cb98ee3d588b68162024cce582189adcd517c4
CanasDev <canasdev@danithan.tk>
2021-07-28 15:08:59 +0100

    A

--- src/Commands/info/pingCommand.js
+++ src/Commands/info/pingCommand.js
@@ -25,7 +25,7 @@ async execute(ctx) {
   const lava = this.client.manager.nodes.filter(a => a.stats.uptime !== 0).first()
     const pingEmbed = new EthanEmbed()
     .setTitle("🏓 Pong")
-    .setDescription(`<:clock2:862344276028555264> \`${pingApi}ms\`\n<:internet:797178541702774834> \`${this.client.shards.get(0).latency}ms\`\n<:MongoDB:862343156854423552> \`${pingDB}ms\`\n<:lava:862345050667089950> \`${this.client.lavalinkPings.get(lava.options.identifier).ping}ms\``)
+    .setDescription(`<:clock2:862344276028555264> \`${pingApi}ms\`\n<:internet:797178541702774834> \`${this.client.shards.get(ctx.msg.channel.guild.shard).latency}ms\`\n<:MongoDB:862343156854423552> \`${pingDB}ms\`\n<:lava:862345050667089950> \`Infinity ms\``)
     .setFooter(`${ctx.msg.author.username}#${ctx.msg.author.discriminator}`, ctx.msg.author.dynamicAvatarURL())
     .setColor("RED")
   
